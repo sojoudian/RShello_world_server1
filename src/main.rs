4ee338ed76e465c6ef00264461029e15b8d8bb55
maziar <sojodyan@gmail.com>
2024-10-31 08:40:33 -0400

    update main.rs with Hello World webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // Build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    // Specify the address and port
+    let addr = SocketAddr::from(([127, 0, 0, 1], 5002));
+    println!("Listening on http://{}", addr);
+
+    // Start the server
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
